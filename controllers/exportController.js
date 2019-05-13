d824604ce087bc102d9a0adb7ebfbf2fb1189e47
j.dawe <jack.dawe@outlook.fr>
2019-05-13 11:16:09 +0200

    export CSV,JSON,XML: ok export XLSX: en cours

--- controllers/exportController.js
+++ controllers/exportController.js
@@ -3,6 +3,9 @@ const moment = require('moment');
 const json2csv = require('json2csv').parse;
 const path = require('path');
 
+const json2xml = require('json2xml');
+const json2xlsx = require('json2xlsx');
+const mongoXlsx = require('mongo-xlsx');
 
 
 
@@ -20,10 +23,12 @@ exports.exportToCsv = async function(req,res,model,fields,filename) {
     let csv;
     try {
         csv =  json2csv(data,{fields});
+        res.render('export_success',{});
     } catch (err) {
         return res.status(500).json({err});
 
     }
+
     var myPath = path.join(__dirname,"..","exports",filename + ".csv");
     fs.writeFile(myPath,csv, async function(err) {
 
@@ -40,7 +45,58 @@ exports.exportToCsv = async function(req,res,model,fields,filename) {
 
 };
 
-exports.exportToJson = async function(req,res) {
+exports.exportToXml= async function(req,res,model,filename) {
 
+    var data = await model.find({}).lean();
 
+    try {
+        var xml = json2xml(data);
+
+        res.render('export_success',{});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({err});
+    }
+
+    var myPath = path.join(__dirname,"..","exports",filename + ".xml");
+
+    fs.writeFile(myPath,xml, async function(err) {
+
+        if (err) {
+            return res.json(err).status(500);
+
+        }
+    });
+
+};
+
+exports.exportToJson = async function(req,res,model,filename) {
+    var data = await model.find({}).exec();
+
+    var myPath = path.join(__dirname,"..","exports",filename + ".json");
+
+    fs.writeFile(myPath,data, async function(err) {
+
+        if (err) {
+            return res.json(err).status(500);
+
+        }
+    });
+
+    res.render('export_success',{});
+
+};
+
+exports.exportToXlsx = async function(req,res,model,filename) {
+    var data = await model.find({}).lean();
+    try {
+        mongoXlsx.mongoData2Xlsx(data, model, function(err, data) {
+            console.log('File saved at:', data.fullPath);
+        });
+
+        res.render('export_success',{});
+    } catch (err) {
+        console.log(err);
+        return res.json(err).status(500);
+    }
 };
